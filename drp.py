e493cafa97bde2cc22b1baf8981d576133f52907
mmajewsk <>
2021-11-23 12:11:18 +0000

    Deploying to gh-pages from @ mmajewsk/howisyourphd@90a17e175cbd6607b1cb29e061355cfa3d7bb945 🚀

---
+++ drp.py
@@ -0,0 +1,25 @@
+import os
+import dropbox
+
+token = os.environ.get("DRP_AUTH_TOKEN")
+sec = os.environ.get("TEST_SECRET")
+print(sec)
+print(sec)
+print(sec)
+print(token[:3])
+dbx = dropbox.Dropbox(token)
+
+
+with open("dis.pdf", "wb") as f:
+    metadata, res = dbx.files_download(path="/Apps/Overleaf/phd_thesis_mmajewski/dissertation.pdf")
+    f.write(res.content)
+stream = os.popen('pdfinfo dis.pdf | grep "Pages:"')
+pages = stream.read()
+
+if os.path.exists("dis.pdf"):
+  os.remove("dis.pdf")
+else:
+  print("The file does not exist")
+
+with open("pages.txt", "w") as f:
+    f.write(pages)
